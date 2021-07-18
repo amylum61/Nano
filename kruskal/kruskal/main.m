e717acd4cfff90935a6384f24a6dc50ffd60d2a2
Nan0 <gan88588@163.com>
2021-07-18 13:53:17 +0800

    Initial commit

---
+++ kruskal/kruskal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  kruskal
+//
+//  Created by Admin on 2021/7/18.
+//  Copyright © 2021 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
